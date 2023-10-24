b5217a92b78642158ba54321c5ff8c9db8dd72b8
Esteban Miranda <tebiccr@gmail.com>
2023-10-24 08:59:24 -0300

    subiendo modelo Pet

--- database/migrations/2023_10_10_130846_create_pets_table.php
+++ database/migrations/2023_10_10_130846_create_pets_table.php
@@ -11,16 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('fathers_name');
-            $table->string('mothers_name');
-            $table->date('birthday');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pets');
     }
 };
